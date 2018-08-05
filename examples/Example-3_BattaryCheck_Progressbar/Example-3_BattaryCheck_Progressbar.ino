a64d91d724b7538958fee57cbbfcad0cf1af7753
Dimi <dsiberia9s@protonmail.com>
2018-08-05 13:34:25 +0700

    added example #3

---
+++ examples/Example-3_BattaryCheck_Progressbar/Example-3_BattaryCheck_Progressbar.ino
@@ -0,0 +1,47 @@
+/* After your libraries */
+#include <M5_UI.h>
+
+/* RootVar's for UI elements (note: not edit manually) */
+String UIProgressbar_1mlmhcu = "0";
+String UITextbox_f2hpjlm = "Example #3. BattaryCheck";
+String UITextbox_1g50hx5 = "Progressbar";
+String UITextbox_wm0ptf8 = "Voltage, mV:";
+String UITextbox_gtaetjh = "0";
+
+/* Function for layer default: */
+void LayerFunction_default(String* rootVar) {
+ /* UI Elements */
+ UIProgressbar(15,85,290,"default","Battary tester",&UIProgressbar_1mlmhcu);
+ UITextbox(15,15,290,20,0x0000,"default",&UITextbox_f2hpjlm);
+ UITextbox(15,40,290,20,0x0000,"default",&UITextbox_1g50hx5);
+ UITextbox(15,175,150,20,0x0000,"default",&UITextbox_wm0ptf8);
+ UITextbox(185,175,80,20,0x0000,"default",&UITextbox_gtaetjh);
+ /* To open this layer use: */
+ UILayer("default");
+ // BattaryCheck
+ while(true) {
+  int voltage = analogRead(35) * 3400 / 4096;
+  int percentage = voltage * 100 / 1600;
+  UISet(&UIProgressbar_1mlmhcu, percentage);
+  UISet(&UITextbox_gtaetjh, voltage);
+  delay(500);
+ }
+}
+
+void setup() {
+ /* Prepare M5STACK */
+ M5.begin();
+
+ /* Prepare Wire for FACES */
+ Wire.begin();
+ pinMode(KEYBOARD_INT, INPUT_PULLUP);
+
+ /* Prepare user's I/O. For example pinMode(5, OUTPUT); */
+
+ /* Prepare UI */
+ UIBegin();
+ LayerFunction_default(0);
+}
+
+void loop() { }
+
